a570d469ab155e75b66f797f0e6a3618bfb23577
Alexandre B A Villares <abav@lugaralgum.com>
2020-05-14 09:21:47 -0300

    Update caneta_automatica.py

--- caneta_automatica/caneta_automatica.py
+++ caneta_automatica/caneta_automatica.py
@@ -1,7 +1,27 @@
+# -*- coding: utf-8 -*-
+# Precisa ter essa linha estranha aí em cima, saiba mais em 
+# abav.lugaralgum.com/material-aulas/Processing-Python/modulos
+
+"""
+Módulo caneta automática tem funções que lembram a tartaruga do logo
+permitem desenhar "arrastando uma caneta" em um papel virtual
+
+Chame inicie_caneta() depois de size()
+
+# começa com o traço ligado
+suba_caneta()  # desliga o traço
+baixe_caneta()  # volta o traço
+
+ande(passos)  # em pixels
+vire(angulo)  # em graus
+esquerda()  # 90 graus
+direita()  # 90 graus
+"""
+
 def inicie_caneta():
     global caneta
     caneta = True  # a caneta começa abaixada
-    translate(width / 2, height / 2)
+    translate(width / 2, height /su 2)
     rotate(HALF_PI)  # rotate(radians(90))
 
 def suba_caneta():
