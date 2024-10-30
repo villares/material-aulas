a79ab5c9d008bbca9eec99358c391a398a7b6eab
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2024-10-29 23:30:03 -0300

    Update for_loop.py

--- Processing-Python-py5/estudos-para-exemplos/for_loop.py
+++ Processing-Python-py5/estudos-para-exemplos/for_loop.py
@@ -1,11 +1,27 @@
-size(200, 200)
-no_fill()         # formas sem preenchimento
-rect_mode(CENTER) # desenhar retângulos e quadrados pelo centro
 
-tamanhos = [20, 40, 80, 160]
-for t in tamanhos:
-    square(100, 100, t)
+size(400, 400)
+
+no_fill()  # formas sem preenchimento
+
+cores = [
+    color(0, 0, 200),    # azul
+    color(255, 255, 0),  # amarelo
+    color(0, 128, 0),    # verde
+    color(255, 0, 255),  # magenta
+    color(0, 255, 255),  # ciano
+    ]
+x = 40
+for c in cores:
+    fill(c)
+    rect(x, 40, 64, 320)
+    x = x + 64
     
-save('for_t_in_tamanhos.png')
+save('for_c_in_cores.png')
            
-           
+# size(400, 400)
+# diâmetros = [300, 250, 200, 150, 100, 50]  # uma lista de números
+# for d in diâmetros:  # atribua a variável `d` para cada diâmetro
+#     circle(200, 200, d)  # desenha a cada volta um círculo o diâmetro `d` da vez
+#     
+#     
+# save('for_circulos.png')
