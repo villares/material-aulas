f342b57956ce9a2c2a7da55b08b64c1150dec3ed
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2025-05-25 22:04:36 -0300

    Update related_scan.py

--- scripts/related_scan.py
+++ scripts/related_scan.py
@@ -2,8 +2,11 @@ import re
 from pathlib import Path
 
 import py5
+from markdown import markdown as md_to_html
 
 summary = Path("/home/villares/GitHub/material-aulas/Processing-Python-py5/README.md")
+
+li_template = """<li class="thumbnail"><img alt="{img_alt}" class="thumbnail" src="{img_src}"/>{entry}</li>""".format
 
 def get_linked_page(txt):
     pattern = r'^\s*-\s*(?:(\d+(?:\.\d+)*)\s+)?.*?\[.*?\]\(([^)]+)\).*$'
@@ -30,19 +33,31 @@ def get_first_image(lines):
 def get_external_page(page):
     return py5.load_strings(page)
 
+li_block = False
 for entry in summary.read_text().splitlines():
     if entry.startswith('#'):
-        print(entry)
+        print(md_to_html(entry))
+        if li_block:
+            print('</ul>')
+            li_block = False       
     else:
         number, page = get_linked_page(entry)
         if page:
-            if 'http' not in page:
-                page = summary.parent / page
+            if 'http' in page:
+                page_path = page # URL 
+            else:
+                page_path = summary.parent / page # local path
             try:
-                lines = py5.load_strings(page)        
+                lines = py5.load_strings(page_path)        
                 img = get_first_image(lines)
                 if img and img.startswith('assets'):
                     img = 'https://abav.lugaralgum.com/material-aulas/Processing-Python-py5/' + img
             except Exception as e:
                 img = str(e)
-            print(number, img)
+            #print(number, img)
+            if not li_block:
+                li_block = True
+                print('<ul>')
+            entry = entry.strip(' -0123456789.') # .replace('.md', '.html')
+            li = li_template(img_alt=number, img_src=img, entry=md_to_html(entry))
+            print(li)
