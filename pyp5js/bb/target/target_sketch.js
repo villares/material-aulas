073304e9415fc0758adc421f0d6f33c9c6f36f2f
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2025-06-24 15:41:07 -0300

    argh!
    adding the contour context man hack

--- pyp5js/bb/target/target_sketch.js
+++ pyp5js/bb/target/target_sketch.js
@@ -340,12 +340,19 @@ def curve_point(*args):
 def curve_tangent(*args):
     return _P5_INSTANCE.curveTangent(*args)
 
-def begin_contour(*args):
-    return _P5_INSTANCE.beginContour(*args)
+class begin_contour():
+    def __init__(self):
+        _P5_INSTANCE.beginContour()
+        
+    def __enter__(self):
+        pass
+    
+    def __exit__(self,  exc_type, exc_value, exc_tb):
+        _P5_INSTANCE.endContour()
 
 class begin_shape():
-    def __init__(self):
-        _P5_INSTANCE.beginShape()
+    def __init__(self, *args):
+        _P5_INSTANCE.beginShape(*args)
 
     def __enter__(self):
         pass
